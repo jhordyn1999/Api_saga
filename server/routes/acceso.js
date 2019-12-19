704290ce055201511e762139cffebc171a680840
jhordyn1999 <jhordynalexander10@gmail.com>
2019-12-19 18:51:31 -0500

    qaqa

--- server/routes/acceso.js
+++ server/routes/acceso.js
@@ -83,5 +83,57 @@ router.get('/acceso/ultimoAcceso', (req,res) => {
 
 
 
+router.get('/iluminacion', (req,res) => {
+    mysqlConnection.query('SELECT * FROM iluminacion', (err, rows, fields) =>{
+        
+        
+        if(!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+            
+        }
+    });
+});
 
+router.post('/iluminacion/InsertarIluminacion', (req,res) => {
+
+    const {_area} = req.body;
+    let respuesta={};
+    const query = `
+    CALL pa_insertarIluminacion(?);
+    `;
+    mysqlConnection.query(query,[_area],(err,rows,fields) => {
+        if(!err) {
+        //    respuesta['Respuesta'] = {'Response' : 'OK','StatusCode':1};
+            respuesta['respuesta']=rows[0];
+            res.json(respuesta);
+        } else {
+           // respuesta['Respuesta'] = {'Response' : 'NO','StatusCode':400};
+            console.log(err);
+
+            
+        }
+    });
+});
+router.get('/iluminacion/ultimaIluminacion', (req,res) => {
+
+
+    let respuesta={};
+    const query = `
+    CALL pa_ultimaIluminacion();
+    `;
+    mysqlConnection.query(query,(err,rows,fields) => {
+        if(!err) {
+        //    respuesta['Respuesta'] = {'Response' : 'OK','StatusCode':1};
+            respuesta['respuesta']=rows[0];
+            res.json(respuesta);
+        } else {
+           // respuesta['Respuesta'] = {'Response' : 'NO','StatusCode':400};
+            console.log(err);
+
+            
+        }
+    });
+});
 module.exports = router;
